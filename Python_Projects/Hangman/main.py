87e78b6ed889076972e06904a8ac7e9979ae8b17
Davin Chang <davinchang12@gmail.com>
2021-10-11 15:26:47 +0700

    change some unused code

--- Python_Projects/Hangman/main.py
+++ Python_Projects/Hangman/main.py
@@ -1,10 +1,5 @@
-import time
+from hangman import Hangman
 
 if __name__ == "__main__":
-    print("Welcome to Hangman game!")
-    name = input("Enter your name")
-    time.sleep(2)
-    print("The game is about to start!")
-    time.sleep(3)
-
-    
+    game = Hangman()
+    game.play()
