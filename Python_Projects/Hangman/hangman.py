5f39d7ffafc4df331a79fcbfadcbe277146765c6
Davin Chang <davinchang12@gmail.com>
2021-10-11 14:59:32 +0700

    added play function

--- Python_Projects/Hangman/hangman.py
+++ Python_Projects/Hangman/hangman.py
@@ -10,10 +10,12 @@ class Hangman():
         self.guess_word = ["dog", "cat", "house", "film", "car", "planet", "bicycle", "laptop"]
         self.word = random.choice(self.guess_word)
         self.length = len(self.word)
-        self.count = 0
         self.display = "_" * self.length
         self.guessed_word = []
         self.play_game = ""
+        
+        self.count = 0
+        self.limit = 5
 
     def play_again(self):
         """
@@ -34,5 +36,84 @@ class Hangman():
             print("Thanks for playing !")
             exit()
 
+    def wrong_ans_print(self):
+        """
+        Print to screen if user guess wrong letter.
+        """
+        print("Wrong guess. " + str(self.limit - self.count) + " last guess remaining\n")
+
     def play(self):
-        pass
+        """
+        Play hangman game.
+        """
+        guess = input("Guess the word : " + self.display + "\nHint:" + self.length + " word(s).\nEnter your guess : ").strip()
+        if (len(guess == 0) or len(guess >= 2) or (guess <= 9)):
+            print("Invalid input, try a letter!")
+            self.play()
+        elif guess in self.word:
+            self.guessed_word.extend([guess])
+            index = self.word.find(guess)
+            self.word = self.word[:index] + "_" + self.word[index + 1:]
+            self.display = self.display[:index] + "_" + self.display[index + 1:]
+            print(self.display + "\n")
+        elif guess in self.guessed_word:
+            print("Try another letter.\n")
+        else:
+            self.count += 1
+            if self.count == 1:
+                time.sleep(1)
+                print("   _____ \n"
+                    "  |      \n"
+                    "  |      \n"
+                    "  |      \n"
+                    "  |      \n"
+                    "  |      \n"
+                    "  |      \n"
+                    "__|__\n")
+                self.wrong_ans_print()
+            elif self.count == 2:
+                time.sleep(1)
+                print("   _____ \n"
+                    "  |     | \n"
+                    "  |     |\n"
+                    "  |      \n"
+                    "  |      \n"
+                    "  |      \n"
+                    "  |      \n"
+                    "__|__\n")
+                self.wrong_ans_print()
+            elif self.count == 3:
+                time.sleep(1)
+                print("   _____ \n"
+                        "  |     | \n"
+                        "  |     |\n"
+                        "  |     | \n"
+                        "  |      \n"
+                        "  |      \n"
+                        "  |      \n"
+                        "__|__\n")
+                self.wrong_ans_print()
+            elif self.count == 4:
+                time.sleep(1)
+                print("   _____ \n"
+                    "  |     | \n"
+                    "  |     |\n"
+                    "  |     | \n"
+                    "  |     O \n"
+                    "  |      \n"
+                    "  |      \n"
+                    "__|__\n")
+                self.wrong_ans_print()
+            elif self.count == 5:
+                time.sleep(1)
+                print("   _____ \n"
+                    "  |     | \n"
+                    "  |     |\n"
+                    "  |     | \n"
+                    "  |     O \n"
+                    "  |    /|\ \n"
+                    "  |    / \ \n"
+                    "__|__\n")
+                print("Wrong guess. You are hanged!!!\n")
+                print("The word was: ", self.already_guessed, self.word)
+                self.play_loop()
