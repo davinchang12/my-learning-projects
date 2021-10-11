e11d4fadd1326f96dde6f565ae15f8aa053b6426
Davin Chang <davinchang12@gmail.com>
2021-10-11 14:34:37 +0700

    hangman inital commit

---
+++ Python_Projects/Hangman/hangman.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+class Hangman():
+    def __init__(self):
+        pass
+
+    def play(self):
+        pass
---
+++ Python_Projects/Hangman/main.py
@@ -0,0 +1,8 @@
+import time
+
+if __name__ == "__main__":
+    print("Welcome to Hangman game!")
+    name = input("Enter your name")
+    time.sleep(2)
+    print("The game is about to start!")
+    time.sleep(3)
