149c79e96e4dd964e0f5bb86f84750cd58b3b615
Davin Chang <davinchang12@gmail.com>
2021-10-13 11:50:28 +0700

    add function comments

--- Python_Projects/Rock_Paper_Scissors/rps.py
+++ Python_Projects/Rock_Paper_Scissors/rps.py
@@ -6,15 +6,27 @@ class RPS:
         self.computer_choice = random.choice(self.choices)
 
     def set_player_choice(self):
+        """
+        Set player choice
+        """
         pass
 
     def win(self):
-        pass
+        """
+        Return win message
+        """
+        return "You win!"
 
     def lose(self):
-        pass
+        """
+        Return lose message
+        """
+        return "You lose!"
 
     def check(self, player_choice):
+        """
+        Check if player have winning / losing choice
+        """
         if(player_choice == "rock" and self.computer_choice == "paper"):
             self.lose()
         elif(player_choice == "rock" and self.computer_choice == "scissors"):
@@ -29,5 +41,8 @@ class RPS:
             self.win()
 
     def play_again(self):
+        """
+        Play the game again
+        """
         pass
 
