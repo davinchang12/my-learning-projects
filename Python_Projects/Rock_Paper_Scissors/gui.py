0557669d4b460bc2a74f3d12e542ecb7f82677c2
Davin Chang <davinchang12@gmail.com>
2021-10-14 09:03:51 +0700

    add base gui

---
+++ Python_Projects/Rock_Paper_Scissors/gui.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("400x400")
+root.resizable(0,0)
+root.title("Rock, Paper, Scissors")
+root.config(bg="seashell3")
+
+Label(root, text = 'Rock, Paper ,Scissors' , font='arial 20 bold', bg = 'seashell2').pack()
