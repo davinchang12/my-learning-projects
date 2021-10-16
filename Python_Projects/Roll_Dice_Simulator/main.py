b48ab3724e5b44fb5e0765b744ee0b1fb96531b4
Davin Chang <davinchang12@gmail.com>
2021-10-16 21:25:20 +0700

    add dice

--- Python_Projects/Roll_Dice_Simulator/main.py
+++ Python_Projects/Roll_Dice_Simulator/main.py
@@ -5,3 +5,20 @@ import random
 root = tkinter.Tk()
 root.geometry('400x400')
 root.title('Roll the Dice')
+
+dice = [
+    'img\dice1.png',
+    'img\dice2.png',
+    'img\dice3.png',
+    'img\dice4.png',
+    'img\dice5.png',
+    'img\dice6.png'
+]
+
+DiceImage = ImageTk.PhotoImage(Image.open(r'img\dice6.png'))
+ImageLabel = tkinter.Label(root, image=DiceImage)
+ImageLabel.image = DiceImage
+
+ImageLabel.pack(expand=True)
+
+root.mainloop()
