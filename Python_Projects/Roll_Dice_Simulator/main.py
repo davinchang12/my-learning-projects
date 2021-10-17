8f921ed6c16b560cc2862adc63f114943e64f86d
Davin Chang <davinchang12@gmail.com>
2021-10-17 07:59:47 +0700

    making class

--- Python_Projects/Roll_Dice_Simulator/main.py
+++ Python_Projects/Roll_Dice_Simulator/main.py
@@ -2,32 +2,46 @@ import tkinter
 from PIL import Image, ImageTk
 import random
 
-root = tkinter.Tk()
-root.geometry('400x400')
-root.title('Roll the Dice')
+class RollDice():
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.geometry('400x400')
+        self.root.title('Roll the Dice')
 
-dice = [
-    'img\dice1.png',
-    'img\dice2.png',
-    'img\dice3.png',
-    'img\dice4.png',
-    'img\dice5.png',
-    'img\dice6.png'
-]
+        self.dice = [
+            'img\dice1.png',
+            'img\dice2.png',
+            'img\dice3.png',
+            'img\dice4.png',
+            'img\dice5.png',
+            'img\dice6.png'
+        ]
 
-DiceImage = ImageTk.PhotoImage(Image.open(random.choice(dice)))
-ImageLabel = tkinter.Label(root, image=DiceImage)
-ImageLabel.image = DiceImage
+    def rolling_dice(self):
+        """
+        Roll new dice
+        """
+        self.DiceImage = ImageTk.PhotoImage(Image.open(random.choice(self.dice)))
+        self.ImageDice.configure(image=self.DiceImage)
+        self.ImageDice.image = self.DiceImage
 
-ImageLabel.pack(expand=True)
+    def play(self):
+        """
+        Make GUI
+        """
+        self.DiceImage = ImageTk.PhotoImage(Image.open(random.choice(self.dice)))
+        self.ImageDice = tkinter.Label(self.root, image=self.DiceImage)
+        self.ImageDice.image = self.DiceImage
 
-def rolling_dice():
-    DiceImage = ImageTk.PhotoImage(Image.open(random.choice(dice)))
-    ImageLabel.configure(image=DiceImage)
-    ImageLabel.image = DiceImage
+        self.button = tkinter.Button(self.root, text="Roll the dice", fg="blue", command=self.rolling_dice)
+        
+        self.ImageDice.pack(expand=True)
+        self.button.pack()
+        
+        self.root.mainloop()
 
-button = tkinter.Button(root, text="Roll the dice", fg="blue", command=rolling_dice)
 
-button.pack()
 
-root.mainloop()
+if __name__ == "__main__":
+    roll_dice = RollDice()
+    roll_dice.play()
