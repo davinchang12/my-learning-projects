9758518cf39aac8ca09c73f2b4e77785f7f85062
Davin Chang <davinchang12@gmail.com>
2021-10-17 21:15:56 +0700

    [add]: add provider and check if signed in

--- Flutter/login_register/lib/main.dart
+++ Flutter/login_register/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:login_register/authentication_service.dart';
+import 'package:login_register/home.dart';
+import 'package:login_register/signin.dart';
+import 'package:provider/provider.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
@@ -10,23 +15,36 @@ Future<void> main() async{
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login Register',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) => context.read<AuthenticationService>().authStateChanges
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Login Register',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: AuthenticationWrapper(),
       ),
-      home: AuthenticationWrapper(),
     );
   }
 }
 
 class AuthenticationWrapper extends StatelessWidget {
-  const AuthenticationWrapper({ Key? key }) : super(key: key);
+  const AuthenticationWrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    final firebaseUser = context.watch<User>();
+    
+    if(firebaseUser != null) {
+      return HomePage();
+    }
+    return SignInPage();
   }
 }
