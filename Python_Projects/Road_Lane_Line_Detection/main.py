226149476247c34f50848f939ceeb413218d23c2
Davin Chang <davinchang12@gmail.com>
2021-10-20 21:40:12 +0700

    [add]: add process_image func

--- Python_Projects/Road_Lane_Line_Detection/main.py
+++ Python_Projects/Road_Lane_Line_Detection/main.py
@@ -6,6 +6,8 @@ import matplotlib.image as mpimg
 from moviepy.editor import VideoFileClip
 import math
 
+from numpy.core.defchararray import lower
+
 def interested_region(img, vertices):
     """
     Return ROI
@@ -126,6 +128,36 @@ def process_image(image):
     """
     Return processed each frame of video to detect lane
     """
-    pass
+    global first_frame
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    img_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+
+    lower_yellow = np.array([20, 100, 100], dtype=np.uint8)
+    upper_yellow = np.array([30, 255, 255], dtype=np.uint8)
+
+    mask_yellow = cv2.inRange(img_hsv, lower_yellow, upper_yellow)
+    mask_white = cv2.inRange(gray_image, 200, 255)
+    mask_yw = cv2.bitwise_or(mask_white, mask_yellow)
+    mask_yw_image = cv2.bitwise_and(gray_image, mask_yw)
+
+    gauss_gray = cv2.GaussianBlur(mask_yw_image, (5, 5), 0)
+    canny_edges = cv2.Canny(gauss_gray, 50, 150)
+
+    imshape = image.shape
+    
+    lower_left = [imshape[1] / 9, imshape[0]]
+    lower_right = [imshape[1] - imshape[1] / 9, imshape[0]]
+    top_left = [imshape[1] / 2 - imshape[1] / 8, imshape[0] / 2 + imshape[0] / 10]
+    top_right = [imshape[1] / 2 + imshape[1] / 8, imshape[0] / 2 + imshape[0] / 10]
+    vertices = [np.array([lower_left, top_left, top_right, lower_right], dtype=np.int32)]
+    roi_images = interested_region(canny_edges, vertices)
+
+    theta = np.pi / 180
+
+    line_image = hough_lines(roi_images, 4, theta, 30, 100, 180)
+    result = weighted_img(line_image, image, a=0.8, beta=1., theta=0.)
+    
+    return result
 
 
