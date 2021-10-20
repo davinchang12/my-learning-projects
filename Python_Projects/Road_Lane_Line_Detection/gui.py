d8b4a7f4df60bdcf33b70a260c0d784c26b4d9ba
Davin Chang <davinchang12@gmail.com>
2021-10-20 21:46:57 +0700

    [add]: add base function

--- Python_Projects/Road_Lane_Line_Detection/gui.py
+++ Python_Projects/Road_Lane_Line_Detection/gui.py
@@ -4,3 +4,22 @@ import cv2
 from PIL import Image, ImageTK
 import os
 import numpy as np
+
+global last_frame1
+global last_frame2
+global cap1
+global cap2
+
+last_frame1 = np.zeros((480, 640, 3), dtype=np.uint8)
+last_frame2 = np.zeros((480, 640, 3), dtype=np.uint8)
+
+cap1 = cv2.VideoCapture("vid/input.mp4")
+cap2 = cv2.VideoCapture("vid/output.mp4")
+
+def show_vid():
+    pass
+
+def show_vid2():
+    pass
+
+
