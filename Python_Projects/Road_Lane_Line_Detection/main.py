fc9b4564e982486d681af80942ee236dab4af17e
Davin Chang <davinchang12@gmail.com>
2021-10-20 21:42:12 +0700

    [maint]: delete unused imports

--- Python_Projects/Road_Lane_Line_Detection/main.py
+++ Python_Projects/Road_Lane_Line_Detection/main.py
@@ -1,10 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-import os
-import matplotlib.image as mpimg
-from moviepy.editor import VideoFileClip
-import math
 
 from numpy.core.defchararray import lower
 
