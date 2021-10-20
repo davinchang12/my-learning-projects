45ec4eb3d8ec65f276b04c2aae0e49d6fc1316e9
Davin Chang <davinchang12@gmail.com>
2021-10-20 11:35:31 +0700

    [add]: add base function needed

--- Python_Projects/Road_Lane_Line_Detection/main.py
+++ Python_Projects/Road_Lane_Line_Detection/main.py
@@ -5,3 +5,39 @@ import os
 import matplotlib.image as mpimg
 from moviepy.editor import VideoFileClip
 import math
+
+def interested_region(img, vertices):
+    """
+    Return ROI
+
+    Applying frame masking and find region of interest
+    """
+    pass
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    """
+    Return Line
+
+    Applying Hough Transform
+    """
+    pass
+
+def lines_drawing(img, lines, color=[255, 0, 0], thickness=6):
+    """
+    Create two lines in each frame
+    """
+    pass
+
+def weighted_img(img, initial_img, alpha=0.8, beta=1., lamb=0.):
+    """
+    Return weighted image
+    """
+    pass
+
+def process_image(image):
+    """
+    Return processed each frame of video to detect lane
+    """
+    pass
+
+
