85839b1e0257e3b7323c26bad51b017f7af4a3a5
Davin Chang <davinchang12@gmail.com>
2021-10-22 22:51:46 +0700

    [add]: add provider

---
+++ Flutter/provider/lib/theme_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  Color mainColor = Colors.blue;
+
+  void changeThemeColor(Color color) {
+    mainColor = color;
+    notifyListeners();
+  }
+}
