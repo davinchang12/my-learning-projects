19ad9a157cda11e98e50831809113e6962dae95d
Davin Chang <davinchang12@gmail.com>
2021-10-22 23:33:16 +0700

    [add]: added provider for theme color selection

--- Flutter/theme_color_provider/lib/main.dart
+++ Flutter/theme_color_provider/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:provider/provider.dart';
+import 'package:theme_color_provider/theme_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +10,142 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) => MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MainScaffold(),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MainScaffold extends StatelessWidget {
+  const MainScaffold({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: _ThemedAppBar(
+        title: const Text('Theme provider example'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.colorize),
+            onPressed: () => _showColorPicker(context),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Consumer<ThemeProvider>(
+              builder: (context, themeProvider, child) => Text(
+                'Text',
+                style: Theme.of(context).textTheme.headline2?.copyWith(
+                      color: themeProvider.mainColor,
+                    ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Consumer<ThemeProvider>(
+              builder: (context, themeProvider, child) => Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                      color: themeProvider.mainColor,
+                    ),
+              ),
+            ),
+            Consumer<ThemeProvider>(
+              builder: (context, themeProvider, child) => Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: themeProvider.mainColor,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+            Consumer<ThemeProvider>(
+              builder: (context, themeProvider, child) => Slider(
+                activeColor: themeProvider.mainColor,
+                inactiveColor: themeProvider.mainColor.withOpacity(0.5),
+                value: 0,
+                onChanged: (newValue) {},
+              ),
+            ),
+            Consumer<ThemeProvider>(
+              builder: (context, themeProvider, child) => Switch(
+                activeColor: themeProvider.mainColor,
+                value: true,
+                onChanged: (newValue) {},
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _showColorPicker(BuildContext context) {
+    ThemeProvider themeProvider =
+        Provider.of<ThemeProvider>(context, listen: false);
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              titlePadding: const EdgeInsets.all(0.0),
+              contentPadding: const EdgeInsets.all(0.0),
+              content: Wrap(
+                children: [
+                  ColorPicker(
+                    pickerColor: themeProvider.mainColor,
+                    onColorChanged: (color) =>
+                        themeProvider.changeThemeColor(color),
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Close'),
+                )
+              ],
+            ));
+  }
+}
+
+class _ThemedAppBar extends StatelessWidget with PreferredSizeWidget {
+  final Widget? title;
+  final List<Widget>? actions;
+  @override
+  final Size preferredSize;
+
+  _ThemedAppBar({
+    Key? key,
+    this.title,
+    this.actions,
+  })  : preferredSize = const Size.fromHeight(kToolbarHeight),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) => AppBar(
+        title: title,
+        actions: actions,
+        backgroundColor: themeProvider.mainColor,
+      ),
     );
   }
 }
