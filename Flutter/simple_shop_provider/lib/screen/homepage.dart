465374a7a5d6e3d7fc92d3295e8eb2043eddd6a1
Davin Chang <davinchang12@gmail.com>
2021-10-24 23:07:50 +0700

    [maint]: add provider and conditions

--- Flutter/simple_shop_provider/lib/screen/homepage.dart
+++ Flutter/simple_shop_provider/lib/screen/homepage.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_shop_provider/cart/cartprovider.dart';
 import 'package:simple_shop_provider/cart/item.dart';
 import 'package:simple_shop_provider/screen/cartpage.dart';
+
+int itemCount = Item.itemShop.length;
+List<bool> _checked = <bool>[];
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,8 +15,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    for(var i = 0; i < itemCount; i++) {
+      _checked.add(false);
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    CartProvider cartProvider = Provider.of<CartProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Simple Shop Provider'),
@@ -19,7 +34,7 @@ class _HomePageState extends State<HomePage> {
           IconButton(
             onPressed: () {
               Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => CartPage()));
+                  context, MaterialPageRoute(builder: (context) => const CartPage()));
             },
             icon: const Icon(Icons.shopping_cart),
           ),
@@ -29,7 +44,6 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         itemBuilder: (context, index) {
           final Item item = Item.itemShop[index];
-          bool _checked = false;
           return InkWell(
             child: Card(
               child: Container(
@@ -38,12 +52,18 @@ class _HomePageState extends State<HomePage> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(item.name),
-                    Text(item.price.toString()),
+                    Text("\$" + item.price.toString()),
                     IconButton(
-                      icon: _checked ? Icon(Icons.check) : Icon(Icons.add),
+                      icon: _checked.elementAt(index) ? const Icon(Icons.check) : const Icon(Icons.add),
                       onPressed: () {
                         setState(() {
-                          _checked = !_checked;
+                          if (_checked[index] == true) {
+                            cartProvider.delItemCart(item.id);
+                          } else {
+                            cartProvider.addItemCart(item.id, item.name, item.price);
+                          }
+
+                          _checked[index] = !_checked.elementAt(index);
                         });
                       },
                     ),
@@ -53,7 +73,7 @@ class _HomePageState extends State<HomePage> {
             ),
           );
         },
-        itemCount: Item.itemShop.length,
+        itemCount: itemCount
       ),
     );
   }
