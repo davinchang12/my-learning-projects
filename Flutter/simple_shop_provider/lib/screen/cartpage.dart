cd715de7b26d82d89cc03c105aeae8029314adcb
Davin Chang <davinchang12@gmail.com>
2021-10-24 23:08:11 +0700

    [add]: add cartpage

--- Flutter/simple_shop_provider/lib/screen/cartpage.dart
+++ Flutter/simple_shop_provider/lib/screen/cartpage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_shop_provider/cart/cartprovider.dart';
+import 'package:simple_shop_provider/cart/item.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -9,8 +12,66 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Cart"),
       ),
-      body: const Center(
-        child: Text("Hello!"),
+      body: Consumer<CartProvider>(
+        builder: (context, cartProvider, child) => ListView.builder(
+            itemBuilder: (context, index) {
+              final Item item = cartProvider.itemCart[index];
+              return InkWell(
+                child: Card(
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(item.name),
+                        Text("\$" + item.price.toString()),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+            itemCount: cartProvider.itemCart.length),
+      ),
+      bottomSheet: Consumer<CartProvider>(
+        builder: (context, cartProvider, child) => Container(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  const Text(
+                    "Total :",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    cartProvider.getTotalPrice().toString(),
+                    style: const TextStyle(
+                      fontSize: 36,
+                    ),
+                  ),
+                ],
+              ),
+              TextButton(
+                  onPressed: () {
+                    final snackBar = SnackBar(
+                      content: const Text('Item checked out'),
+                    );
+
+                    // Find the ScaffoldMessenger in the widget tree
+                    // and use it to show a SnackBar.
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+                  child: const Text("Checkout")),
+            ],
+          ),
+        ),
       ),
     );
   }
