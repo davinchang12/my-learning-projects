8edcf4693cb1240e8687d10ff2ed0696642d1909
Davin Chang <davinchang12@gmail.com>
2021-10-28 14:29:41 +0700

    [add]: add func

--- Notebooks/python-project-color-detection/main.py
+++ Notebooks/python-project-color-detection/main.py
@@ -13,6 +13,15 @@ def draw_function(event, x, y, flags, param):
         g = int(g)
         r = int(r)
 
+def get_color_name(r, g, b):
+    minimum = 10000
+    for i in range(len(col_name)):
+        d = abs(r - int(col_name.loc[i, 'r'])) + abs(r - int(col_name.loc[i, 'g'])) + abs(r - int(col_name.loc[i, 'b']))
+        if d <= minimum :
+            minimum = d
+            cname = col_name.loc[i, 'color_name']
+    return cname
+
 img_path = 'img/colorpic.jpg'
 img = cv2.imread(img_path)
 
