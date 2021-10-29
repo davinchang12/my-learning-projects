7eab6522140b9079d0898f9915a59ef9742d12b2
Davin Chang <davinchang12@gmail.com>
2021-10-29 20:13:10 +0700

    [add]: add back button widget

---
+++ Flutter/design_pattern/lib/widgets/platform_back_button.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class PlatformBackButton extends StatelessWidget {
+  final Color color;
+
+  const PlatformBackButton({
+    required this.color,
+    Key? key
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Platform.isAndroid ? Icons.arrow_back : Icons.arrow_back_ios,
+      ),
+      color: color,
+      onPressed: () => Navigator.pop(context),
+    );
+  }
+}
