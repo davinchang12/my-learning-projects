c6b4618b4a46d4f11d02552a79b220dd3bd44fac
Davin Chang <davinchang12@gmail.com>
2021-10-29 18:26:18 +0700

    [maint]: add const

--- Flutter/design_pattern/lib/screens/main_menu/widgets/main_menu_header.dart
+++ Flutter/design_pattern/lib/screens/main_menu/widgets/main_menu_header.dart
@@ -5,26 +5,24 @@ class MainMenuHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Design Pattern',
-            style: TextStyle(
-              fontSize:36,
-              fontWeight: FontWeight.bold
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        Text(
+          'Design Pattern',
+          style: TextStyle(
+            fontSize:36,
+            fontWeight: FontWeight.bold
           ),
-          const SizedBox(height:12),
-          Text(
-            'with Flutter',
-            style: TextStyle(
-              fontSize:24,
-            ),
-          )
-        ],
-      ),
+        ),
+        SizedBox(height:12),
+        Text(
+          'with Flutter',
+          style: TextStyle(
+            fontSize:24,
+          ),
+        )
+      ],
     );
   }
 }
