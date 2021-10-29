90b7343e44f46d68cd1a08ec3004f81e9bca6448
Davin Chang <davinchang12@gmail.com>
2021-10-29 18:14:02 +0700

    [add]: add theme

--- Flutter/design_pattern/lib/theme.dart
+++ Flutter/design_pattern/lib/theme.dart
@@ -1,7 +1,45 @@
-import 'package:design_pattern/constant.dart';
 import 'package:flutter/material.dart';
+
+import 'package:design_pattern/constant.dart';
 
 final ThemeData lightTheme = ThemeData(
   backgroundColor: lightBackgroundColor,
   scaffoldBackgroundColor: lightBackgroundColor,
+  typography: Typography(
+    englishLike: Typography.englishLike2018,
+    dense: Typography.dense2018,
+    tall: Typography.tall2018,
+  ),
+  textTheme: TextTheme(
+    headline1: TextStyle(
+      fontFamily: 'RobotoMedium',
+      fontSize: 34.0,
+      color: Colors.black.withOpacity(0.75),
+    ),
+    headline2: TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 24.0,
+      color: Colors.black.withOpacity(0.65),
+    ),
+    headline3: TextStyle(
+      fontFamily: 'RobotoMedium',
+      fontSize: 20.0,
+      color: Colors.black.withOpacity(0.65),
+    ),
+    subtitle1: TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 16.0,
+      color: Colors.black.withOpacity(0.65),
+    ),
+    bodyText1: TextStyle(
+      fontFamily: 'RobotoMedium',
+      fontSize: 14.0,
+      color: Colors.black.withOpacity(0.65),
+    ),
+    bodyText2: TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 14.0,
+      color: Colors.black.withOpacity(0.65),
+    ),
+  ),
 );
