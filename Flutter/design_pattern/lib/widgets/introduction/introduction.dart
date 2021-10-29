bfff4c7704137676f956be45be8747a63c65675c
Davin Chang <davinchang12@gmail.com>
2021-10-30 00:09:16 +0700

    [maint]: fix error button

---
+++ Flutter/design_pattern/lib/widgets/introduction/introduction.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Introduction extends StatelessWidget {
+  const Introduction({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Hello world!'),
+    );
+  }
+}
