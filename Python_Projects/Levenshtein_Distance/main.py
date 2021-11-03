d7ee1aeabc618c0e1b04900c1676f705e51eb7a8
Davin Chang <davinchang12@gmail.com>
2021-11-03 14:09:47 +0700

    [add]: add func

--- Python_Projects/Levenshtein_Distance/main.py
+++ Python_Projects/Levenshtein_Distance/main.py
@@ -1,4 +1,4 @@
-def iterative_levenshtein():
+def iterative_levenshtein(s, t):
     """ 
         iterative_levenshtein(s, t) -> ldist
         ldist is the Levenshtein distance between the strings 
@@ -7,5 +7,30 @@ def iterative_levenshtein():
         distance between the first i characters of s and the 
         first j characters of t
     """
+    rows = len(s) + 1
+    cols = len(t) + 1
+    dist = [[0 for _ in range(cols)] for _ in range(rows)]
     
-    pass
+    for i in range(1, rows):
+        dist[i][0] = i
+    
+    for i in range(1, cols):
+        dist[0][i] = i
+
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row-1] == t[col-1]:
+                cost = 0
+            else:
+                cost = 1
+            
+            dist[row][col] = min(dist[row-1][col] + 1,      # deletion
+                                 dist[row][col-1] + 1,      # insertion
+                                 dist[row-1][col-1] + cost) # substitution
+    for r in range(rows):
+        print(dist[r])
+
+    return dist[row][col]
+
+
+print(iterative_levenshtein("dog", "dogs"))
