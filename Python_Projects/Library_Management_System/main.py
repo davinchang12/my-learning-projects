f3fff4330ef30bc1f15b9873e999314102d463f7
Davin Chang <davinchang12@gmail.com>
2021-11-04 19:49:58 +0700

    [add] base main view

--- Python_Projects/Library_Management_System/main.py
+++ Python_Projects/Library_Management_System/main.py
@@ -1,3 +1,22 @@
+# Please add database inorder to run this projects.
+# Code for MySQL
+"""
+CREATE DATABASE dbLibrary;
+USE dbLibrary;
+
+CREATE TABLE tblBooks(
+    bid VARCHAR(20) PRIMARY KEY, 
+    title VARCHAR(30), 
+    author VARCHAR(30), 
+    status VARCHAR(30)
+);
+
+CREATE TABLE tblBooksIssued(
+    bid VARCHAR(20) PRIMARY KEY, 
+    issuedto VARCHAR(30)
+);
+"""
+
 from tkinter import *
 from PIL import ImageTk,Image #PIL -> Pillow
 import pymysql
@@ -6,4 +25,19 @@ from addbook import *
 from deletebook import *
 from viewbook import *
 from issuebook import *
+
+db_pass = ""
+db_user = "root"
+db_host = "localhost"
+db = "dbLibrary"
+
+con = pymysql.connect(host=db_host, user=db_user, password=db_pass, database=db)
+
+cur = con.cursor()
+
+root = Tk()
+root.title("Library")
+root.minsize(width=400, height=400)
+root.geometry("600x500")
+root.mainloop()
 
