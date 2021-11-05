283145a9f5fccbbb974eda1cfda0cac33f7e7aac
Davin Chang <davinchang12@gmail.com>
2021-11-05 11:50:52 +0700

    [maint]: fix button func and remove unused imports

--- Python_Projects/Library_Management_System/main.py
+++ Python_Projects/Library_Management_System/main.py
@@ -17,10 +17,8 @@ CREATE TABLE tblBooksIssued(
 );
 """
 from resources.app_view import *
+from resources.connections import *
 
-from tkinter import *
-from PIL import ImageTk,Image #PIL -> Pillow
-import pymysql
 from tkinter import messagebox
 from addbook import *
 from deletebook import *
@@ -28,14 +26,9 @@ from viewbook import *
 from issuebook import *
 from returnbook import *
 
-db_pass = ""
-db_user = "root"
-db_host = "localhost"
-db = "dbLibrary"
+import PIL.Image
+import PIL.ImageTk
 
-con = pymysql.connect(host=db_host, user=db_user, password=db_pass, database=db)
-
-cur = con.cursor()
 
 root = Tk()
 root.title("Library")
@@ -45,7 +38,7 @@ root.geometry("600x500")
 same = True
 n = 0.25
 # Adding a background image
-background_image = Image.open("img/lib.jpg")
+background_image = PIL.Image.open("img/lib.jpg")
 [imageSizeWidth, imageSizeHeight] = background_image.size
 
 newImageSizeWidth = int(imageSizeWidth * n)
@@ -54,19 +47,19 @@ if same :
 else :
     newImageSizeHeight = int(imageSizeHeight / n)
 
-background_image = background_image.resize((newImageSizeWidth, newImageSizeHeight), Image.ANTIALIAS)
-img = ImageTk.PhotoImage(background_image)
+background_image = background_image.resize((newImageSizeWidth, newImageSizeHeight), PIL.Image.ANTIALIAS)
+img = PIL.ImageTk.PhotoImage(background_image)
 Canvas1 = Canvas(root)
 Canvas1.create_image(300, 340, image = img)
 Canvas1.config(bg="white", width=newImageSizeWidth, height=newImageSizeHeight)
 Canvas1.pack(expand=True, fill=BOTH)
 
 header(root, "Local Library")
-button(root, "Add book details", addBook, 0.28, 0.4)
-button(root, "Delete book", deleteBook, 0.28, 0.5)
-button(root, "View book list", viewBook, 0.28, 0.6)
-button(root, "Issue book to Student", issueBook, 0.28, 0.7)
-button(root, "Return book", returnBook, 0.28, 0.8)
+button(root, "Add book details", 'black', 'white', 0.45, 0.1, addBook, 0.28, 0.4)
+button(root, "Delete book", 'black', 'white', 0.45, 0.1, deleteBook, 0.28, 0.5)
+button(root, "View book list", 'black', 'white', 0.45, 0.1, viewBook, 0.28, 0.6)
+button(root, "Issue book to Student", 'black','white', 0.45, 0.1, issueBook, 0.28, 0.7)
+button(root, "Return book", 'black', 'white', 0.45, 0.1, returnBook, 0.28, 0.8)
 
 root.mainloop()
 
