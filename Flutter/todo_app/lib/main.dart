fb9926218e6c3f413cdd51d8fabf2738a5f8d94e
Davin Chang <davinchang12@gmail.com>
2021-11-07 11:22:59 +0700

    [maint]: add homepage

--- Flutter/todo_app/lib/main.dart
+++ Flutter/todo_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ToDo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: const HomePage(),
     );
   }
 }
